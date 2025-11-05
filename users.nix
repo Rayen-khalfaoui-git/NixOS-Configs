19e7650bf7d3cb61164da2668f394e1976b2fea4
installer <installer@gmail.com>
2025-11-05 23:00:45 +0100

    commit

---
+++ users.nix
@@ -0,0 +1,69 @@
+# Users - NOTE: Packages defined on this will be on current user only
+
+{ pkgs, username, ... }:
+
+let
+  inherit (import ./variables.nix) gitUsername;
+in
+{
+  users = { 
+    mutableUsers = true;
+    users."${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = "${gitUsername}";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+        "video" 
+        "input" 
+        "audio"
+      ];
+
+    # define user packages here
+    packages = with pkgs; [
+      ];
+    };
+    
+    defaultUserShell = pkgs.zsh;
+  }; 
+  
+  environment.shells = with pkgs; [ zsh ];
+  environment.systemPackages = with pkgs; [ lsd fzf ]; 
+    
+  programs = {
+  zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = ""; # leave empty, we’ll source the theme manually
+    };
+  };
+};
+
+  environment.etc."zsh/p10k.zsh".source = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+
+
+environment.shellInit = ''
+  # Load Powerlevel10k if available
+  [[ -r /etc/zsh/p10k.zsh ]] && source /etc/zsh/p10k.zsh
+
+  # Enable autosuggestions
+  if [ -f ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+    source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+  fi
+
+  # Enable syntax highlighting
+  if [ -f ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+    source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  fi
+
+  # Make autosuggestions appear in a dim color
+  ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+'';
+
+}
---
+++ variables.nix
@@ -0,0 +1,18 @@
+# Variables
+
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Rayen-khalfaoui-git";
+  gitEmail = "rayen.khalfaoui.contact@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser 
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "fr";
+}
